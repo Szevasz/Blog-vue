515743d9c713fab7879a5e5191fcee654cd1f32a
Szevasz <Szevasz@163.com>
2023-10-07 21:47:00 +0800

    first commit

---
+++ src/layouts/components/AdminFooter.vue
@@ -0,0 +1,20 @@
+<template>
+    <footer class="footer">
+        Copyright © 2023. All rights reserved. Provided by&nbsp;<a class="szevasz" href="" target="_blank">Szevasz</a>
+    </footer>
+</template>
+
+<style scoped>
+.footer {
+    @apply text-gray-500 text-sm justify-center flex;
+    padding-top: 1.5rem;
+    padding-bottom: 1.5rem;
+    background-color: #fff;
+    margin-top: 5rem;
+}
+
+.szevasz {
+    text-decoration: underline;
+    color: rgb(52, 152, 219);
+}
+</style>
